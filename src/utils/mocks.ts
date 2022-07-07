51402567018d3c1255b5d69217d8bcf4a9b4182b
isaacviannadev <isaacsvianna@gmail.com>
2022-07-07 07:47:04 -0300

    Mock de Lideres e clientes

---
+++ src/utils/mocks.ts
@@ -0,0 +1,67 @@
+export const lidersMock = [
+  {
+    id: 1,
+    nome: 'Rodrigo Lemos',
+    cargo: 'CEO e Founder',
+    bio: 'Um dos fortes nomes de SEO no Brasil, com expertise em estratégias de tráfego orgânico em players como, por exemplo, startup Méliuz, os portais Letras.mus.br, Cifra Club, iDinheiro, entre outros. Atualmente, é CEO da Netlinks cuidando da visão do negócio, professor de SEO e apaixonado por novos negócios.',
+    link: 'https://www.linkedin.com/in/rodrigo-lemos-a8a8b8b4/',
+  },
+  {
+    id: 2,
+    nome: 'Tiago Faria',
+    cargo: 'CSO e Founder',
+    bio: 'Conhecido no mundo das startups como Aspira, tem uma linda jornada em gigantes como Rock Content, Conta Azul, Kenoby e Gupy, atuando em cargos estratégicos e gestão de times de alta performance. Hoje em dia é responsável por inovação, processos e todo o setor de vendas na Netlinks.',
+    link: 'https://www.linkedin.com/in/tiago-faria-a8a8b8b4/',
+  },
+  {
+    id: 3,
+    nome: 'João Pedro',
+    cargo: 'CTO e Founder',
+    bio: 'João Pedro é um desenvolvedor de software com expertise em desenvolvimento de software, estratégia de tráfego orgânico, SEO e marketing digital. Atualmente, é CTO da Netlinks cuidando da visão do negócio, professor de SEO e apaixonado por novos negócios.',
+    link: 'https://www.linkedin.com/in/joao-pedro-a8a8b8b4/',
+  },
+  {
+    id: 4,
+    nome: 'Ana Paula',
+    cargo: 'CFO e Founder',
+    bio: 'Ana Paula é um desenvolvedor de software com expertise em desenvolvimento de software, estratégia de tráfego orgânico, SEO e marketing digital. Atualmente, é CFO da Netlinks cuidando da visão do negócio, professor de SEO e apaixonado por novos negócios.',
+    link: 'https://www.linkedin.com/in/ana-paula-a8a8b8b4/',
+  },
+  {
+    id: 5,
+    nome: 'Rodrigo Lemos',
+    cargo: 'CEO e Founder',
+    bio: 'Um dos fortes nomes de SEO no Brasil, com expertise em estratégias de tráfego orgânico em players como, por exemplo, startup Méliuz, os portais Letras.mus.br, Cifra Club, iDinheiro, entre outros. Atualmente, é CEO da Netlinks cuidando da visão do negócio, professor de SEO e apaixonado por novos negócios.',
+    link: 'https://www.linkedin.com/in/rodrigo-lemos-a8a8b8b4/',
+  },
+  {
+    id: 6,
+    nome: 'Tiago Faria',
+    cargo: 'CSO e Founder',
+    bio: 'Conhecido no mundo das startups como Aspira, tem uma linda jornada em gigantes como Rock Content, Conta Azul, Kenoby e Gupy, atuando em cargos estratégicos e gestão de times de alta performance. Hoje em dia é responsável por inovação, processos e todo o setor de vendas na Netlinks.',
+    link: 'https://www.linkedin.com/in/tiago-faria-a8a8b8b4/',
+  },
+];
+
+export const clientesMock = [
+  {
+    id: 1,
+    nome: 'aluguefoco.com.br',
+    img: '/public/Group 1.png',
+  },
+  {
+    id: 2,
+    nome: 'precato.com.br',
+    img: '/public/Group 2.png',
+  },
+  {
+    id: 3,
+    nome: 'virtuspay.com.br',
+    img: '/public/Group 3.png',
+  },
+  {
+    id: 4,
+    nome: 'scopi.com.br',
+    img: '/public/Group 4.png',
+  },
+];
